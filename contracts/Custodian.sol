e337a6a309d5cf15d3fe000c885f8e0f6e92fd81
Lawrence Xia <xialairei@gmail.com>
2021-11-28 01:13:45 -0800

    job spec plumbing - works up to EA call

--- contracts/Custodian.sol
+++ contracts/Custodian.sol
@@ -9,9 +9,9 @@ contract Custodian is ChainlinkClient, Ownable {
   using Chainlink for Chainlink.Request;
   using Strings for uint256;
 
-  address private oracle;
-  bytes32 private jobId;
-  uint256 private fee;
+  address public oracle;
+  bytes32 public jobId;
+  uint256 public fee;
 
   struct Request {
     address sender;
@@ -28,19 +28,19 @@ contract Custodian is ChainlinkClient, Ownable {
   function setJobConfig(
     address _link,
     address _oracle,
-    bytes32 _jobId,
+    string memory _jobId,
     uint256 _fee
   ) public onlyOwner {
     setChainlinkToken(_link);
     oracle = _oracle;
-    jobId = _jobId;
+    jobId = stringToBytes32(_jobId);
     fee = _fee;
   }
 
   function verify(uint256 tokenId) public {
     address sender = address(this);
 
-    require(!requestPending[sender], "Sender already has a pending request");
+    // require(!requestPending[sender], "Sender already has a pending request");
 
     Chainlink.Request memory request = buildChainlinkRequest(
       jobId,
@@ -48,17 +48,17 @@ contract Custodian is ChainlinkClient, Ownable {
       this.fulfill.selector
     );
     request.add("tokenId", tokenId.toString());
-    request.addBytes("address", abi.encodePacked(sender));
+    //request.addBytes("address", abi.encodePacked(sender));
 
     bytes32 requestId = sendChainlinkRequestTo(oracle, request, fee);
     requests[requestId] = Request(sender, tokenId);
     requestPending[sender] = true;
   }
 
-  function fulfill(bytes32 requestId, bool doesOwn) external {
+  function fulfill(bytes32 requestId, address owner) external {
     Request storage request = requests[requestId];
 
-    if (doesOwn) {
+    if (request.sender == owner) {
       owners[request.tokenId] = request.sender;
     }
 
@@ -67,5 +67,16 @@ contract Custodian is ChainlinkClient, Ownable {
 
   function ownerOf(uint256 tokenId) public view returns (bool) {
     return owners[tokenId] == address(this);
+  }
+
+  function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+
+    assembly { // solhint-disable-line no-inline-assembly
+      result := mload(add(source, 32))
+    }
   }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,12 +7,51 @@ import { HardhatUserConfig, task } from "hardhat/config"
 dotenv.config()
 ethers.utils.Logger.setLogLevel(ethers.utils.Logger.levels.ERROR)
 
+task("deployOracle", "Deploys the oracle contract")
+  .addPositionalParam("link", "Address of link on the custodian contract")
+  .setAction( async (args, hre) => {
+  const factory = await hre.ethers.getContractFactory("Oracle")
+  const contract = await factory.deploy(args.link, { gasLimit: 2100000 })
+
+  console.log("Contract deployed at: ", contract.address)
+})
+
+// npx hardhat deployOracle 0x326C977E6efc84E512bB9C30f76E30c160eD06FB --network mumbai
+
+task("oracleSetFulfillmentPermission")
+  .addPositionalParam("address", "Address of the oracle")
+  .addPositionalParam("nodeAddress", "Address of the chainlink node")
+  .setAction( async (args, hre) => {
+    const contract = await hre.ethers.getContractAt("Oracle", args.address)
+    const transaction = await contract.setFulfillmentPermission(
+      args.nodeAddress,
+      true
+    )
+
+    console.log("Permission set: ", transaction.hash)
+  })
+// npx hardhat oracleSetFulfillmentPermission 0x14DD47ceb5070F66B1d5e7d07Fc9DfBd3082b7F8 0x644d78e293993A9c3C0600Cd335EC1077163C81a --network mumbai
+
+task("oracleOwner", "Verifies that user owns a token")
+  .addPositionalParam("contract", "The address of the oracle contract")
+  .setAction(async (args, hre) => {
+    const contract = await hre.ethers.getContractAt("Oracle", args.contract)
+    const result = await contract.isOwner()
+
+    console.log("Owner value: ", result)
+  })
+
+// npx hardhat oracleOwner 0x206Cc4d96cd769d6C30Cf1E83AA90bAd30B45767
+
 task("deployCustodian", "Deploys the custodian contract", async (_, hre) => {
   const factory = await hre.ethers.getContractFactory("Custodian")
   const contract = await factory.deploy({ gasLimit: 2100000 })
 
   console.log("Contract deployed at: ", contract.address)
 })
+
+// npx hardhat deployCustodian --network mumbai
+//0x2C3349AAb732d0967Ac7dDB096b91b5e1c02714a
 
 task("deployFakeNFT", "Deploys the FakeNFT contract", async (_, hre) => {
   const factory = await hre.ethers.getContractFactory("FakeNFT")
@@ -21,6 +60,10 @@ task("deployFakeNFT", "Deploys the FakeNFT contract", async (_, hre) => {
   console.log("Contract deployed at: ", contract.address)
 })
 
+// npx hardhat deployFakeNFT --network rinkeby
+//0x8Adb9842a8526b3e285Aa289B0Ceb217709e8551
+
+
 task("setJobConfig", "Sets the job config on the custodian contract")
   .addPositionalParam("address", "Address of the custodian contract")
   .addPositionalParam("link", "Address of link on the custodian contract")
@@ -28,7 +71,8 @@ task("setJobConfig", "Sets the job config on the custodian contract")
   .addPositionalParam("jobId", "The oracle job to run")
   .addPositionalParam("fee", "The oracle fee to pay in LINK")
   .setAction(async (args, hre) => {
-    const contract = await hre.ethers.getContractAt("Custodian", args.contract)
+    //const jobId = ethers.utils.parseBytes32String(args.jobId)
+    const contract = await hre.ethers.getContractAt("Custodian", args.address)
     const transaction = await contract.setJobConfig(
       args.link,
       args.oracle,
@@ -39,16 +83,57 @@ task("setJobConfig", "Sets the job config on the custodian contract")
     console.log("Job config set: ", transaction.hash)
   })
 
+
+// external job id --> remove hyphens 0eec7e1dd0d2476ca1a872dfb6633f02
+// npx hardhat setJobConfig 0x2C3349AAb732d0967Ac7dDB096b91b5e1c02714a 0x326C977E6efc84E512bB9C30f76E30c160eD06FB 0x14DD47ceb5070F66B1d5e7d07Fc9DfBd3082b7F8 69dad7b2102f48b8b118e47580876fe6 0.1 --network mumbai
+
+task("getOracle", "Verifies that user owns a token")
+  .addPositionalParam("contract", "The address of the Custodian contract")
+  .setAction(async (args, hre) => {
+    const contract = await hre.ethers.getContractAt("Custodian", args.contract)
+    const result = await contract.oracle()
+
+    console.log("Oracle value: ", result)
+  })
+
+  // npx hardhat getOracle 0x2C3349AAb732d0967Ac7dDB096b91b5e1c02714a --network mumbai
+
+task("getJobId", "Verifies that user owns a token")
+  .addPositionalParam("contract", "The address of the Custodian contract")
+  .setAction(async (args, hre) => {
+    const contract = await hre.ethers.getContractAt("Custodian", args.contract)
+    const result = await contract.jobId()
+
+    console.log("Oracle value: ", result)
+  })
+
+  // npx hardhat getJobId 0x2C3349AAb732d0967Ac7dDB096b91b5e1c02714a --network mumbai
+
+  task("getFee", "Verifies that user owns a token")
+  .addPositionalParam("contract", "The address of the Custodian contract")
+  .setAction(async (args, hre) => {
+    const contract = await hre.ethers.getContractAt("Custodian", args.contract)
+    const result = await contract.fee()
+
+    console.log("Oracle fee: ", result)
+  })
+
+  // npx hardhat getFee 0x2C3349AAb732d0967Ac7dDB096b91b5e1c02714a --network mumbai
+
 task("verify", "Verifies that user owns a token")
   .addPositionalParam("contract", "The address of the Custodian contract")
   .addPositionalParam("address", "The address to check")
   .addPositionalParam("tokenId", "The token ID to verify")
   .setAction(async (args, hre) => {
     const contract = await hre.ethers.getContractAt("Custodian", args.contract)
-    const transaction = await contract.verify(args.tokenId)
+    const transaction = await contract.verify(args.tokenId, { gasLimit: 2100000 })
 
     console.log("Verification transaction completed: ", transaction.hash)
   })
+
+
+// npx hardhat verify 0x2C3349AAb732d0967Ac7dDB096b91b5e1c02714a 0x326C977E6efc84E512bB9C30f76E30c160eD06FB  1 --network mumbai
+
 
 task(
   "ownerOf",
@@ -89,7 +174,11 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
+        version: "0.6.6",
+      },
+      {
         version: "0.8.4",
+        settings: { },
       },
     ],
   },
